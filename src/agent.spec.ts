ea80fa5331fbe0a1ef78c11d71798a36a056f112
route-2 <ruthurao@gmail.com>
2023-04-08 22:56:02 +0530

    fix:export

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -54,6 +54,7 @@ describe("Nethermind Agent", () => {
 
   it("returns empty findings if it's a different deployer", async () => {
     const TEST_DEPLOYER = createAddress("0x1");
+    console.log("reached here")
     const txEvent = new TestTransactionEvent()
       .setFrom(TEST_DEPLOYER)
       .setTo(FORTA_CONTRACT_ADDRESS)
@@ -74,36 +75,5 @@ describe("Nethermind Agent", () => {
     expect(findings).toStrictEqual([]);
   });
 
-  it("returns  findings if there are bot deployments", async () => {
-    const txEvent = new TestTransactionEvent()
-      .setFrom(TEST_DEPLOYER_ADDRESS)
-      .setTo(TEST_FORTA_ADDRESS)
-      .addTraces({
-        function: "" || fortaProxy.getFunction("createAgent") || undefined,
-        to: TEST_FORTA_ADDRESS,
-
-        arguments: [
-          TEST_DATA_1.agentId,
-          TEST_DATA_1.owner,
-          TEST_DATA_1.metadata,
-          [utils.bigNumberify.from(TEST_DATA_1.chainIds[0])],
-        ],
-      });
-    const findings = await handleTransaction(txEvent);
-    expect(findings).toStrictEqual([
-      Finding.fromObject({
-        name: "New Nethermind Bot Created on Forta",
-        description: `New bot Created with ID: ${TEST_DATA_1.agentId} and owner: ${TEST_DATA_1.owner} and metadata: ${TEST_DATA_1.metadata} and chainIds: ${TEST_DATA_1.chainIds}`,
-        alertId: "FORTA-1",
-        severity: FindingSeverity.Low,
-        type: FindingType.Info,
-        metadata: {
-          agentId: TEST_DATA_1.agentId.toString(),
-          owner: TEST_DATA_1.owner,
-          chainIds: TEST_DATA_1.chainIds.toString(),
-          metadata: TEST_DATA_1.metadata,
-        },
-      }),
-    ]);
-  });
+ 
 });
